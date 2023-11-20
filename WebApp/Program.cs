95723cb2151cba7c44bc0a3baa682e7185ed997d
lovagithub <lova.juhlin@outlook.com>
2023-11-20 15:32:50 +0100

    Add project files.

---
+++ WebApp/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Contexts;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DataContext>(x =>x.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");    
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.Run();
---
+++ WebApp_Tests/DatabaseTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WebApp_Tests;
+
+public class DatabaseTests
+{
+    [Fact]
+    public void ConnectionString_SqlServer_Should_be_a_ConnectionString_To_AzureSqlDatabase()
+    {
+        //Arrange
+        var expected = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\LovaJ\\OneDrive\\Dokument\\database.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True";
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        // Act
+        var result = configuration.GetConnectionString("SqlServer");
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
