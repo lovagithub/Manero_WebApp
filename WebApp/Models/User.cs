fe2e6f0f3dafa69e1352330dc1739bc1e1906b85
lovagithub <lova.juhlin@outlook.com>
2023-11-21 13:46:22 +0100

    12/11

---
+++ WebApp/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Update.Internal;
+
+namespace WebApp.Models;
+
+public class UserSchema
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+
+}
+public class UserEntity
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+
+
+    public static implicit operator UserEntity(UserSchema schema)
+    {
+        return new UserEntity
+        {
+            FirstName = schema.FirstName,
+            LastName = schema.LastName,
+            Email = schema.Email
+        };
+    }
+}
+
---
+++ WebApp_Tests/UserTests.cs
@@ -0,0 +1,28 @@
+﻿using WebApp.Models;
+
+namespace WebApp_Tests;
+
+public class UserTests
+{
+    [Fact]
+    public void UserSchema_Should_ConvertT_UserEntity()
+    {
+        // Arrange
+        UserSchema schema = new UserSchema()
+        {
+            FirstName = "Lova",
+            LastName = "Juhlin",
+            Email = "lova.juhlin@learnet.se"
+        };
+
+        // Act
+        UserEntity entity = schema;
+
+        // Assert
+        Assert.NotNull(entity);
+        Assert.IsType<UserEntity>(entity);
+        Assert.Equal(schema.FirstName, entity.FirstName);   
+        Assert.Equal(schema.LastName, entity.LastName);
+        Assert.Equal(schema.Email, entity.Email);
+    }
+}
